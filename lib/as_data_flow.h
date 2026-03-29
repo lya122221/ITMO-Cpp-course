c3bb0a4d3574f59d7df078aa391fd7e869490aa6
lya122221 <kipriyanov_2006@mail.ru>
2026-03-29 16:52:24 +0300

    Fixed AsDataFlow adapter

--- lib/as_data_flow.h
+++ lib/as_data_flow.h
@@ -6,10 +6,17 @@ class AsDataFlowAdapter {
 public:
   AsDataFlowAdapter(Source& source) : source_(source) {}
 
-  auto begin() {
+  using iterator = decltype(std::begin(std::declval<Source&>()));
+  using const_iterator = decltype(std::begin(std::declval<const Source&>()));
+  using value_type = std::decay_t<decltype(*std::begin(std::declval<Source&>()))>;
+  using reference = value_type&;
+  using const_reference = const value_type&;
+  using size_type = std::size_t;
+
+  auto begin() const {
     return std::begin(source_);
   }
-  auto end() {
+  auto end() const {
     return std::end(source_);
   }
 private:
