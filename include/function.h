6b539f13f74084744d0b596952aa5c297dba391e
lya122221 <kipriyanov_2006@mail.ru>
2026-04-25 15:54:29 +0300

    Fixed class function

--- include/function.h
+++ include/function.h
@@ -1,3 +1,4 @@
+#pragma once
 #include <memory>
 
 template <typename T>
@@ -6,12 +7,49 @@ class Function;
 template <typename R, typename... Args>
 class Function<R(Args...)> {
 public:
-  Function() = default;
+  Function() : callable_(nullptr) {}
   template <typename T>
   Function(T&& f) : callable_(new Derived<std::decay_t<T>>(std::forward<T>(f))) {}
   ~Function() { delete callable_; }
 
+  Function(const Function& other) {
+    if (other.callable_) {
+      callable_ = other.callable_->GetCopy(); 
+    } else {
+      callable_ = nullptr;
+    }
+  }
+
+  Function(Function&& other) noexcept {
+    callable_ = other.callable_;
+    other.callable_ = nullptr;
+  }
+
+  Function& operator=(const Function& other) {
+    if (this != &other) {
+      delete callable_;
+      if (other.callable_) {
+        callable_ = other.callable_->GetCopy();
+      } else {
+        callable_ = nullptr;
+      }
+    }
+    return *this;
+  }
+
+  Function& operator=(Function&& other) noexcept {
+    if (this != &other) {
+      delete callable_;
+      callable_ = other.callable_;
+      other.callable_ = nullptr;
+    }
+    return *this;
+  }
+
   R operator()(Args... args) const {
+    if (!callable_) {
+      throw std::bad_function_call();
+    }
     return callable_->invoke(std::forward<Args>(args)...);
   }
 private:
