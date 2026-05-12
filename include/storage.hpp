484afbc7415054f6cd7b2184dd1fb9e57fbe2ac9
lya122221 <kipriyanov_2006@mail.ru>
2026-05-12 17:24:23 +0300

    Added basic methods for storage

--- include/storage.hpp
+++ include/storage.hpp
@@ -8,6 +8,8 @@
 #include <vector>
 #include <optional>
 
+namespace kvdb {
+
 class Storage {
 public:
   using Value = std::variant<
@@ -15,9 +17,24 @@ public:
     std::list<std::string>, 
     std::set<std::string>
   >;
+
+  void Set(const std::string& key, const std::string& value) {
+    storage_[key] = value;
+  }
+
+  std::optional<Value> Get(const std::string& key) {
+    auto key_it = storage_.find(key);
+    if (key_it == storage_.end()) {
+      return std::nullopt;
+    }
+
+    return key_it->second;
+  }
 private:
   std::unordered_map<std::string, Value> storage_;
 
   size_t max_memory_;
   size_t curr_memory_used_;
 };
+
+}
