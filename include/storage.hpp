93899b0350460b5fe2a61478ef4576c10847c4f5
lya122221 <kipriyanov_2006@mail.ru>
2026-05-13 00:57:19 +0300

    Added ttl for storage values

--- include/storage.hpp
+++ include/storage.hpp
@@ -6,7 +6,8 @@
 #include <set>
 #include <memory>
 #include <vector>
-#include <optional>
+#include <expected>
+#include <chrono>
 
 namespace kvdb {
 
@@ -17,24 +18,54 @@ public:
     std::list<std::string>, 
     std::set<std::string>
   >;
+  using ExpiredTime = std::chrono::steady_clock::time_point;
 
-  void Set(const std::string& key, const std::string& value) {
-    storage_[key] = value;
+  void Set(const std::string& key, const std::string& value, std::chrono::steady_clock::time_point ttl = std::chrono::steady_clock::time_point::max()) {
+    storage_[key] = { value, ttl };
   }
 
-  std::optional<Value> Get(const std::string& key) {
+  std::expected<Value, std::string> Get(const std::string& key) {
     auto key_it = storage_.find(key);
     if (key_it == storage_.end()) {
-      return std::nullopt;
+      return std::unexpected("(error) Invalid key");
     }
 
-    return key_it->second;
+    if (IsExpired(key)) {
+      return std::unexpected("(error) Key expired");
+    }
+
+    return (key_it->second).first;
   }
+
+  std::chrono::seconds GetTtl(const std::string& key) {
+    auto key_it = storage_.find(key);
+    if (key_it == storage_.end()) {
+      return std::chrono::seconds(0);
+    }
+
+    ExpiredTime expired_time = (key_it->second).second;
+    if (expired_time != std::chrono::steady_clock::time_point::max()) {
+      return std::chrono::duration_cast<std::chrono::seconds>(expired_time - std::chrono::steady_clock::now());
+    } else {
+      return std::chrono::seconds(0);
+    }
+  }
+
 private:
-  std::unordered_map<std::string, Value> storage_;
+  std::unordered_map<std::string, std::pair<Value, ExpiredTime>> storage_;
 
   size_t max_memory_;
   size_t curr_memory_used_;
+
+  bool IsExpired(const std::string& key) {
+    auto now = std::chrono::steady_clock::now();
+
+    if (now >= storage_[key].second) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 };
 
 }
